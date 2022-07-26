c03a756e5b6fb236426e1e4446cfc8aae57fc1a0
k-taro-reccos <tarou3806@gmail.com>
2022-07-26 11:27:37 +0900

    implement event creation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,56 @@
-import React from 'react';
+import React, { useReducer, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import reducer from "../reducers";
+
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, []);
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+
+  const addEvent = (e) => {
+    e.preventDefault();
+    dispatch(
+      {
+        type: 'CREATE_EVENT',
+        title,
+        body
+      }
+    )
+    setTitle("")
+    setBody("")
+  };
+
+  console.log({state})
+
   return (
     <div className="container-fluid">
       <h4>イベント作成フォーム</h4>
       <form>
         <div className="form-group">
           <label htmlFor="formEventTitle">タイトル</label>
-          <input className="form-control" type="text" id="formEventTitle" />
+          <input
+            className="form-control"
+            type="text"
+            id="formEventTitle"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
         </div>
         <div className="form-group">
           <label htmlFor="formEventTitle">ボディー</label>
-          <textarea className="form-control" type="text" id="formEventBody" />
+          <textarea
+            className="form-control"
+            type="text"
+            id="formEventBody"
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
+          />
         </div>
 
-        <button className="btn btn-primary">イベントを作成する</button>
+        <button className="btn btn-primary" onClick={addEvent}>
+          イベントを作成する
+        </button>
         <button className="btn btn-danger">全てのイベントを削除する</button>
       </form>
 
@@ -29,12 +64,10 @@ const App = () => {
             <th></th>
           </tr>
         </thead>
-        <tbody>
-          
-        </tbody>
+        <tbody></tbody>
       </table>
     </div>
   );
-}
+};
 
 export default App;
